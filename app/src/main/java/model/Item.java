d0b39163fb3d1a52eb67b98214d813557e89494a
Jake Brunton <106359025+zero-senpai@users.noreply.github.com>
2024-12-11 10:56:25 -0500

    Add room dependencies

---
+++ app/src/main/java/model/Item.java
@@ -0,0 +1,14 @@
+package model;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+public class Item {
+
+    private String id;
+    private String itemName;
+    private int count;
+
+
+    //TODO: Constructor, getter, setter
+
+}
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,16 @@
+package model;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+public class User {
+
+    private String id;
+    private String username;
+    private String password;
+    private boolean hasEditPermissions;
+
+
+    //TODO: Add constructor and getters/setters
+
+}
